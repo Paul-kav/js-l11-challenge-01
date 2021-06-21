75cbafa7ee9e72adfc108befd779043100402a04
Paul-kav <paulkavuma82@gmail.com>
2021-06-21 14:41:32 -0400

    Fetching data

--- js/script.js
+++ js/script.js
@@ -4,9 +4,8 @@ const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
 const getImage = async function() {
-    const res = await fetch(//picsum.photos/v2/list?limit=100.
-        ); 
-    const images = await json();
+    const res = await fetch("https://picsum.photos/v2/list?limit=100"); 
+    const images = await res.json();
     console.log(images);
 };
 getImage();
